5046ecbf58336a751d22e983bbddc36194b8245d
thijsrijkers <thijsrijkers@live.nl>
2021-11-15 16:22:53 +0100

    +Added some more tests

--- Rekenmachine/src/test/java/Rekenmachine/InfixTest.java
+++ Rekenmachine/src/test/java/Rekenmachine/InfixTest.java
@@ -20,20 +20,33 @@ public class InfixTest {
     @Test
     @DisplayName("Error testing of the calculate method in the Infix class")
     void testingInfixError() {
-        Exception exception = assertThrows(NumberFormatException.class, () -> {
+        assertThrows(NumberFormatException.class, () -> {
             infix.calculate("+++1+++");
         });
-
-        String expectedMessage = "For input string";
-        String actualMessage = exception.getMessage();
-
-        assertTrue(actualMessage.contains(expectedMessage));
     }
 
     @Test
-    @DisplayName("Giving Infix correct calculation to return a double")
-    void testingInfixResponse() {
+    @DisplayName("Giving Infix correct add calculation to return a double")
+    void testingInfixAddResponse() {
         assertEquals(2.0, infix.calculate("1+1"));
+    }
+
+    @Test
+    @DisplayName("Giving Infix correct multiple calculation to return a double")
+    void testingInfixMultipleResponse() {
+        assertEquals(1.0, infix.calculate("1*1"));
+    }
+
+    @Test
+    @DisplayName("Giving Infix correct minus calculation to return a double")
+    void testingInfixMinusResponse() {
+        assertEquals(1.0, infix.calculate("2-1"));
+    }
+
+    @Test
+    @DisplayName("Giving Infix correct divide calculation to return a double")
+    void testingInfixDivideResponse() {
+        assertEquals(2.0, infix.calculate("4/2"));
     }
 
 }
--- Rekenmachine/src/test/java/Rekenmachine/PostfixTest.java
+++ Rekenmachine/src/test/java/Rekenmachine/PostfixTest.java
@@ -33,4 +33,29 @@ public class PostfixTest {
         assertEquals(1, postfix.calculate("823*+7/1-"));
     }
 
+
+    @Test
+    @DisplayName("Giving Postfix correct add calculation to return a double")
+    void testingPostfixAddResponse() {
+        assertEquals(2, postfix.calculate("11+"));
+    }
+
+    @Test
+    @DisplayName("Giving Postfix correct multiple calculation to return a double")
+    void testingPostfixMultipleResponse() {
+        assertEquals(1, postfix.calculate("11*"));
+    }
+
+    @Test
+    @DisplayName("Giving Postfix correct minus calculation to return a double")
+    void testingPostfixMinusResponse() {
+        assertEquals(1, postfix.calculate("21-"));
+    }
+
+    @Test
+    @DisplayName("Giving Postfix correct divide calculation to return a double")
+    void testingPostfixDivideResponse() {
+        assertEquals(2, postfix.calculate("42/"));
+    }
+
 }
