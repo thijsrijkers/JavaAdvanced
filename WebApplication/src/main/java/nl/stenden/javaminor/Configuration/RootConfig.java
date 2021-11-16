be313c9ab49e7cb548c2c6b5aabb1633d73d9c39
thijsrijkers <t.rijkers@provrex.nl>
2021-11-16 13:40:17 +0100

    +_

--- WebApplication/src/main/java/nl/stenden/javaminor/Configuration/RootConfig.java
+++ WebApplication/src/main/java/nl/stenden/javaminor/Configuration/RootConfig.java
@@ -12,6 +12,6 @@ public class RootConfig implements WebMvcConfigurer {
 
     @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer contentNegotiationConfigurer) {
-        contentNegotiationConfigurer.defaultContentType(MediaType.APPLICATION_JSON);
+
     }
 }
--- WebApplication/src/main/java/nl/stenden/javaminor/Controller/HelloWorldController.java
+++ WebApplication/src/main/java/nl/stenden/javaminor/Controller/HelloWorldController.java
@@ -1,13 +1,11 @@
 package nl.stenden.javaminor.Controller;
 
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/helloworld")
-@ComponentScan(basePackages = "nl.stenden.javaminor")
 public class HelloWorldController {
 
     @GetMapping()
