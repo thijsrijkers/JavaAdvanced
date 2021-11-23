e361e2e9e6a4d1115f33c62c333da2136609a21f
thijsrijkers <t.rijkers@provrex.nl>
2021-11-23 11:41:27 +0100

    +workable JDBC

---
+++ DatabaseJDBC/src/main/java/Main.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.sql.*;
+
+public class Main {
+    public static void main(String[] args){
+
+        try {
+            Connection connection =DriverManager.getConnection("jdbc:mysql://localhost/databasejdbc?user=root");
+            System.out.println("Connection maded");
+
+            System.out.println("Write DB entry:");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            String input = reader.readLine();
+
+            Statement statement = connection.createStatement();
+            statement.execute("INSERT INTO product VALUES ('"+input+"')") ;
+            ResultSet result = statement.executeQuery("SELECT * FROM product");
+
+            while(result.next())
+            {
+                System.out.println(result.getString("productNaam"));
+            }
+
+            connection.close();
+
+        } catch (Exception exception) {
+            System.out.println(exception.getMessage());
+        }
+    }
+}
