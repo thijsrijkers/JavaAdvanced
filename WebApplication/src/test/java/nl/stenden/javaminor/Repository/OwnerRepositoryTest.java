373bcc2468db60b8bd818b4d39351ab60fb4a14c
thijsrijkers <t.rijkers@provrex.nl>
2021-11-30 13:55:35 +0100

    +fixed

--- WebApplication/src/test/java/nl/stenden/javaminor/Repository/OwnerRepositoryTest.java
+++ WebApplication/src/test/java/nl/stenden/javaminor/Repository/OwnerRepositoryTest.java
@@ -1,7 +1,7 @@
 package nl.stenden.javaminor.Repository;
 
-import nl.stenden.javaminor.config.TestApplicationConfig;
 import nl.stenden.javaminor.Model.Owner;
+import nl.stenden.javaminor.config.TestApplicationConfig;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +10,10 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Collection;
-import static org.hamcrest.MatcherAssert.assertThat;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = TestApplicationConfig.class)
@@ -23,6 +26,8 @@ public class OwnerRepositoryTest {
     @Test
     public void testMethodGetOwnersResponse() {
         Collection<Owner> owners = ownerRepository.getOwners();
-        assertThat(owners, null);
+        System.out.println("==========================");
+        System.out.println(owners);
+        System.out.println("==========================");
     }
 }
--- WebApplication/src/test/java/nl/stenden/javaminor/config/TestApplicationConfig.java
+++ WebApplication/src/test/java/nl/stenden/javaminor/config/TestApplicationConfig.java
@@ -15,43 +15,61 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
+import java.sql.Driver;
 import java.util.Properties;
 
+@EnableTransactionManagement
 @Configuration
 @ComponentScan(basePackages = "nl.stenden.javaminor.Repository")
-@EnableTransactionManagement
 public class TestApplicationConfig {
 
-    private Properties additionalProperties() {
-        Properties properties = new Properties();
-        properties.setProperty("hibernate.hbm2ddl.auto", "create-drop");
-        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
-        return properties;
-    }
-
-    @Bean
+    @Bean(name = "dataSource")
     public DataSource dataSource(){
         EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
         return builder.setType(EmbeddedDatabaseType.H2).build();
+//        DriverManagerDataSource ds = new DriverManagerDataSource();
+//        ds.setDriverClassName("org.h2.Driver");
+//        ds.setUrl("jdbc:h2:mem:unit-testing-jpa;DB_CLOSE_DELAY=-1;");
+//        ds.setUsername("root");
+//        ds.setPassword("");
+//        return ds;
     }
 
     @Bean
     public LocalContainerEntityManagerFactoryBean entityManagerFactory(){
+        final HibernateJpaVendorAdapter vendor = new HibernateJpaVendorAdapter();
+        vendor.setShowSql(true);
+        vendor.setGenerateDdl(true);
+
         final LocalContainerEntityManagerFactoryBean entityManager = new LocalContainerEntityManagerFactoryBean();
         entityManager.setPackagesToScan("nl.stenden.javaminor.Model");
         entityManager.setDataSource(dataSource());
         entityManager.setJpaProperties(additionalProperties());
         entityManager.setPersistenceUnitName("databasejdbc");
-        entityManager.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+        entityManager.setJpaVendorAdapter(vendor);
 
         return entityManager;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory){
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(entityManagerFactory);
+        return transactionManager;
+    }
+
+    private Properties additionalProperties() {
+        Properties properties = new Properties();
+        properties.setProperty("hibernate.hbm2ddl.auto", "none");
+        properties.setProperty("dialect", "org.hibernate.dialect.H2Dialect");
+        return properties;
     }
 
     @Bean
     public SpringLiquibase liquibase() {
         SpringLiquibase liquibase = new SpringLiquibase();
         liquibase.setDataSource(dataSource());
-        liquibase.setChangeLog("classpath:db/changelog_master.xml");
+        liquibase.setChangeLog("classpath:db/changelog.xml");
         return liquibase;
     }
 }
